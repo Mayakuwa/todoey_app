0306fefaa2bb8209ce14075219706f9f1b151e6e
Maya <melokitty.1003@gmail.com>
2020-11-06 11:58:16 +0900

    add delete task

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -19,9 +19,18 @@ class TaskData extends ChangeNotifier {
   }
 
   void addTask(String newTaskTitle) {
-
     final task = Task(name: newTaskTitle);
     _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
     notifyListeners();
   }
 }
